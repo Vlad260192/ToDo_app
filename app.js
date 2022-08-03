20918de843c0790dbe44874b296f782574982a4c
Vlad260192 <v.kapustianskiy@gmail.com>
2022-08-03 11:43:14 +0300

    commit

--- app.js
+++ app.js
@@ -4,6 +4,45 @@ function createTodoItem(title) {
     checkbox.className = 'checkbox';
 
     const label = document.createElement('label');
+    label.innerText = title;
+    label.className = 'title';
+
+    const editInput = document.createElement('input');
+    editInput.type = 'text';
+    editInput.className = 'textfield';
+
+    const editButton = document.createElement('button');
+    editButton.innerText = 'Изменить';
+    editButton.className = 'edit';
+
+    const deleteButton = document.createElement('button');
+    deleteButton.innerText = 'Удалить';
+    deleteButton.className = 'delete';
+
+    const listItem = document.createElement('li');
+    listItem.className = 'todo-item';
+
+    listItem.appendChild(checkbox);
+    listItem.appendChild(label);
+    listItem.appendChild(editInput);
+    listItem.appendChild(editButton);
+    listItem.appendChild(deleteButton);
+    
+    bindEvents(listItem);
+
+    return listItem;
+
+}
+
+function bindEvents(todoItem) {
+    const checkbox = todoItem.querySelector('checkbox');
+    const editButton = todoItem.querySelector('button.edit');
+    const deleteButton = todoItem.querySelector('button.delete');
+
+    checkbox.addEventListener('change', toggleTodoItem);
+    editButton.addEventListener('click', editTodoItem);
+    deleteButton.addEventListener('click', deleteTodoItem);
+
 }
 
 function addTodoItem(event) {
@@ -11,7 +50,22 @@ function addTodoItem(event) {
 
     if (addInput.value === '') return alert('необходимо ввести название задачи.');
 
-    const listItem = createTodoItem(addInput.value);
+    const todoItem = createTodoItem(addInput.value);
+    todoList.appendChild(todoItem);
+    addInput.value = '';
+}
+
+function toggleTodoItem({ target }) {
+    console.log(target);
+    console.log(this);
+}
+
+function editTodoItem() {
+
+} 
+
+function deleteTodoItem() {
+
 }
 
 const todoForm = document.getElementById('todo-form');
@@ -19,4 +73,4 @@ const addInput = document.getElementById('add-input');
 const todoList = document.getElementById('todo-list');
 const todoItems = document.querySelectorAll('.todo-item');
 
-todoForm.addEventListener('submit', addTodoItem)
+todoForm.addEventListener('submit', addTodoItem);
